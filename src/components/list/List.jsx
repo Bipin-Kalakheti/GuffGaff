3937bda5e4e0d4bcdc7f43eea798437b92e9c973
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-02 22:40:56 -0400

    implement chat list design

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,16 @@
 import "./list.css";
 
 import React from "react";
+import UserInfo from "./userInfo/UserInfo.jsx";
+import ChatList from "./chatList/ChatList.jsx";
 
 function List() {
-  return <div className="list"></div>;
+  return (
+    <div className="list">
+      <UserInfo />
+      <ChatList />
+    </div>
+  );
 }
 
 export default List;
