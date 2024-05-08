0088538a6a8b2e0a565d26fdb5dbf0c0f8ebf7c3
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-08 14:10:05 -0400

    implement zustand state management tool

---
+++ src/lib/userStore.js
@@ -0,0 +1,29 @@
+import { doc, getDoc } from "firebase/firestore";
+import { jsx } from "react/jsx-runtime";
+import { create } from "zustand";
+import { db } from "./firebase";
+
+const useUserStore = create((set) => ({
+  currentUser: null,
+  isLoading: true,
+
+  fetchUserInfo: async (uid) => {
+    if (!uid) return set({ currentUser: null, isLoading: false });
+
+    try {
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        set({ currentUser: docSnap.data(), isLoading: false });
+      } else {
+        set({ currentUser: null, isLoading: false });
+      }
+    } catch (err) {
+      console.log(err);
+      return set({ currentUser: null, isLoading: false });
+    }
+  },
+}));
+
+export default useUserStore;
