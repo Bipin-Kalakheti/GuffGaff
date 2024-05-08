8c5157963ef16a291695ca64d0e90123f3deb667
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-05-08 16:33:00 -0400

    add new chat creation functionality

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,10 +1,23 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import "./addUser.css";
 import { db } from "../../../../lib/firebase";
 import { useState } from "react";
+import { useUserStore } from "../../../../lib/userStore";
 
 const AddUser = () => {
   const [user, setUser] = useState(null);
+
+  const { currentUser } = useUserStore();
 
   const handleSearch = async (e) => {
     e.preventDefault();
@@ -21,7 +34,41 @@ const AddUser = () => {
       if (!querySnapShot.empty) {
         setUser(querySnapShot.docs[0].data());
       }
-      //create a query against the collection.
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAdd = async () => {
+    const chatRef = collection(db, "chat");
+    const userChatsRef = collection(db, "userchats");
+
+    try {
+      const newChatRef = doc(chatRef);
+
+      await setDoc(newChatRef, {
+        createdAt: serverTimestamp(),
+        messages: [],
+      });
+
+      await updateDoc(doc(userChatsRef, user.id), {
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: currentUser.id,
+          updatedAt: Date.now(),
+        }),
+      });
+
+      await updateDoc(doc(userChatsRef, currentUser.id), {
+        chats: arrayUnion({
+          chatId: newChatRef.id,
+          lastMessage: "",
+          receiverId: user.id,
+          updatedAt: Date.now(),
+        }),
+      })
+
     } catch (err) {
       console.log(err);
     }
@@ -39,7 +86,7 @@ const AddUser = () => {
             <img src={user.avatar || "./avatar.png"} alt="" />
             <span>{user.username}</span>
           </div>
-          <button>Add User</button>
+          <button onClick={handleAdd}>Add User</button>
         </div>
       )}
     </div>
